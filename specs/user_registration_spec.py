7df1106dc80c3ed4ec79c902a8bb217cbf005330
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-04 11:19:08 +0100

    First iteration
    10 minutes with ping pong pairing

--- specs/user_registration_spec.py
+++ specs/user_registration_spec.py
@@ -1,18 +1,36 @@
-
 from expects import expect, be_true, raise_error
+
+class RegistrationService(object):
+    def __init__(self):
+        self._users = []
+
+    def register(self, user):
+        if self.has_user(user):
+            raise UserAlreadyRegisteredError()
+        self._users.append(user)
+
+    def has_user(self, user):
+        return user in self._users
+
+
+class UserAlreadyRegisteredError(Exception):
+    pass
+
 
 with description('Register user'):
 
     with before.each:
-        self.nickname = '@foolano'
+        self.user = '@foolano'
+        self.service = RegistrationService()
 
     with it('registers a new user'):
-        # Register a user
-        # Validate that the user is registered
-        pass
+        self.service.register(self.user)
+
+        expect(self.service.has_user(self.user)).to(be_true)
 
     with context('when user already exists'):
         with it('raises error'):
-            # Register a user
-            #
-            pass
+            self.service.register(self.user)
+
+            expect(lambda: self.service.register(self.user)).to(raise_error(UserAlreadyRegisteredError))
+
