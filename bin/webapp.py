010a9f86a1fe3b1b60453a8ae5b399dd6b3e3f50
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-06 16:55:28 +0100

    Add cos publication to HTTP delivery mechanism

--- bin/webapp.py
+++ bin/webapp.py
@@ -38,7 +38,30 @@ class FollowingsHandler(tornado.web.RequestHandler):
     def post(self, nickname):
         try:
             target = json.loads(self.request.body)['nickname']
-            following = users_service.follow_to(nickname, target)
+            users_service.follow_to(nickname, target)
+            self.set_status(httplib.CREATED)
+        except errors.UserDoesNotExistError:
+            self.write({'error': 'User does not exist'})
+            self.set_status(httplib.NOT_FOUND)
+
+
+class CosHandler(tornado.web.RequestHandler):
+
+    def initialize(self, users_service):
+        self.users_service = users_service
+
+    def get(self, nickname):
+        try:
+            cos = users_service.cos_for(nickname)
+            self.write({'result': cos})
+        except errors.UserDoesNotExistError:
+            self.write({'error': 'User does not exist'})
+            self.set_status(httplib.NOT_FOUND)
+
+    def post(self, nickname):
+        try:
+            co = json.loads(self.request.body)['co']
+            users_service.publish_co(nickname, co)
             self.set_status(httplib.CREATED)
         except errors.UserDoesNotExistError:
             self.write({'error': 'User does not exist'})
@@ -49,6 +72,7 @@ users_service = factory.create_users_service()
 application = tornado.web.Application([
     (r'/users', UsersHandler, dict(users_service=users_service)),
     (r'/users/(?P<nickname>[^/]*)/following', FollowingsHandler, dict(users_service=users_service)),
+    (r'/users/(?P<nickname>[^/]*)/cos', CosHandler, dict(users_service=users_service)),
 ], debug=True)
 
 if __name__ == '__main__':
